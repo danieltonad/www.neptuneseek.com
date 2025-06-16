d5621b9bde0ba44426c6d7c79850d91106e6feea
Daniel Solarin <soladan2000@gmail.com>
2025-06-16 04:04:57 -0900

    Add Header component with basic styling and integrate into Home page
    - Created Header component with navigation links
    - Added SCSS styling for Header component
    - Integrated Header component into Home page

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import "././Header.scss";
+
+export default function Header() {
+  return (
+    <div className="neptune-header">
+        <div className="title">
+            Neptune
+        </div>
+
+        <nav>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </nav>
+    </div>
+  )
+}
